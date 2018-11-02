2096f4e316e3ea99fd91bd8590f2f28f936fe9a2
Pooja patel <poojavpatel71@gmail.com>
2018-11-03 01:14:38 +0530

    program to find sum of border elements of a matrix

---
+++ matrix.js
@@ -0,0 +1,23 @@
+//find sum of all border elements of a matrix
+const a = [
+    [1,1,1,1,1],
+    [5,1,1,1,7],
+    [3,1,1,1,5],
+    [2,2,2,2,2]
+]
+
+const nrows = a.length;         //4
+const ncols = a[0].length;      //5
+
+const arrSum = arr => arr.reduce((total,no) => total + no, 0)
+// console.log(arrSum([1,2,3]));
+
+console.log('sum of first row', arrSum(a[0]));               //sum of first row
+console.log('sum of last row', arrSum(a[nrows - 1]));        //sum of last row
+sum = arrSum(a[0]) + arrSum(a[nrows - 1]) ;
+
+for (let i = 1; i < nrows-1 ; i++) {
+    sum += a[i][0] + a[i][ncols - 1];
+}                                                // adding first n last ele of remaining rows
+
+console.log('sum of border elements of matrix is:', sum);
